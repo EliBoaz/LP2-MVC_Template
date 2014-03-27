a08ff23aed8a8cf93cdc091a466261ce8cb00be3
31270425 <31270425@090704-P33S11.macklabs.br>
2014-03-26 22:50:00 -0300

    4

---
+++ src/java/controller/CadastrarUsuarioCommand.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 31270425
+ */
+public class CadastrarUsuarioCommand implements Command {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+    
+}
