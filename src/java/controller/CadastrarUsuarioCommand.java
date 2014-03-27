c073870bde7b2460dc7537278d91d2e1d7217b33
31270425 <31270425@090704-P33S11.macklabs.br>
2014-03-26 22:58:29 -0300

    5

--- src/java/controller/CadastrarUsuarioCommand.java
+++ src/java/controller/CadastrarUsuarioCommand.java
@@ -8,16 +8,19 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import negocio.ManterUsuario;
 
 /**
  *
  * @author 31270425
  */
 public class CadastrarUsuarioCommand implements Command {
+    private ManterUsuario ManterUsuario;
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+        ManterUsuario = new ManterUsuario();
+            
     }
     
 }
